878873859a5ea81fa8ca35a81671371c71fc4b00
kaviyas22 <shreekaviya862@gmail.com>
2025-05-22 21:37:19 +0530

    Add files via upload

---
+++ Company1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package company1;
+
+
+public class Company1 {
+    private static double profit;
+    public static final String Dept="Accounts";
+
+     
+    public static void main(String[] args) {
+        profit=797098;
+        System.out.println(Dept+" shown a profit of: "+profit);
+        
+    }
+    
+}
+
